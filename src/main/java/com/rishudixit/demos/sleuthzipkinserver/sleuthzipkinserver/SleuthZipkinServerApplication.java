b2c859b5dc116d536073f82f46898a15cf37d3a7
RDGitLearn <rishudixit@gmail.com>
2018-11-22 08:10:14 +0530

    First Commit

---
+++ src/main/java/com/rishudixit/demos/sleuthzipkinserver/sleuthzipkinserver/SleuthZipkinServerApplication.java
@@ -0,0 +1,12 @@
+package com.rishudixit.demos.sleuthzipkinserver.sleuthzipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthZipkinServerApplication.class, args);
+	}
+}
